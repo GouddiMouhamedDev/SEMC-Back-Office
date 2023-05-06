f62f744c9add29beb90056260dccf30b8bbfd582
GouddiMouhamedDev <toonmouhamed@gmail.com>
2023-05-06 23:05:10 +0100

    debug add product

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -17,16 +17,17 @@ export class AddComponent {
 
   onProductscreate(prod: any) {
 
-    this.http.post('http://localhost:3000/createproduct', prod).subscribe(
-  () => {
+    this.http.post('http://localhost:3000/createproduct', prod).subscribe({
+      next: () => {
+        this.closeadd(true);
+      },
+      error: (error) => {
+        console.log("Une erreur s'est produite lors de la création du produit : ", error);
+        // Afficher un message d'erreur à l'utilisateur ou effectuer une action en fonction de l'erreur.
+      }
+    });
     
-    this.closeadd(true);
-  },
-  error => {
-    console.log("Une erreur s'est produite lors de la création du produit : ", error);
-    // Afficher un message d'erreur à l'utilisateur ou effectuer une action en fonction de l'erreur.
-  }
-);
+
 
 
    
